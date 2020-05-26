1657974ed079422baaa50748987c44549f854bbd
Justin Vartanian <jvartanian@1904labs.com>
2020-05-26 15:33:53 -0500

    Add LiveData lint rule

--- lint/src/main/java/com/labs1904/lint/LintIssueRegistry.kt
+++ lint/src/main/java/com/labs1904/lint/LintIssueRegistry.kt
@@ -4,12 +4,14 @@ import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.detector.api.Issue
 import com.labs1904.lint.detectors.CompositeDisposableClearDetector
 import com.labs1904.lint.detectors.DisposableClearDetector
+import com.labs1904.lint.detectors.LiveDataDetector
 
 @Suppress("UnstableApiUsage", "Unused")
 class LintIssueRegistry : IssueRegistry() {
 	override val issues: List<Issue>
 		get() = listOf(
 			CompositeDisposableClearDetector.ISSUE,
-			DisposableClearDetector.ISSUE
+			DisposableClearDetector.ISSUE,
+			LiveDataDetector.ISSUE
 		)
 }
