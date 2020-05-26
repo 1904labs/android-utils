8de4ad30faa93e46bdb777538dab4e5ed180c4b5
Justin Vartanian <jvartanian@1904labs.com>
2020-05-26 15:15:08 -0500

    Add CompositeDisposable lint rule

--- lint/src/main/java/com/labs1904/lint/LintIssueRegistry.kt
+++ lint/src/main/java/com/labs1904/lint/LintIssueRegistry.kt
@@ -2,11 +2,12 @@ package com.labs1904.lint
 
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.detector.api.Issue
+import com.labs1904.lint.detectors.CompositeDisposableClearDetector
 
 @Suppress("UnstableApiUsage", "Unused")
 class LintIssueRegistry : IssueRegistry() {
 	override val issues: List<Issue>
 		get() = listOf(
-
+			CompositeDisposableClearDetector.ISSUE
 		)
 }
