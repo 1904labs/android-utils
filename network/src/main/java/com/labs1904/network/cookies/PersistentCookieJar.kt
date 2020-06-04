767c889da3a06706aa169771b4c4f9468a283e94
Justin Vartanian <jvartanian@1904labs.com>
2020-06-03 22:53:24 -0500

    Add PersistentCookieJar

---
+++ network/src/main/java/com/labs1904/network/cookies/PersistentCookieJar.kt
@@ -0,0 +1,67 @@
+package com.labs1904.network.cookies
+
+import com.labs1904.network.cookies.cache.CookieCache
+import okhttp3.Cookie
+import okhttp3.CookieJar
+import okhttp3.HttpUrl
+
+interface MutableCookieJar : CookieJar {
+	fun clear()
+	fun clearSession()
+}
+
+class PersistentCookieJar(
+	private val cookieCache: CookieCache,
+	private val cookiePersistor: CookiePersistor
+) : MutableCookieJar {
+
+	private var loaded = false
+
+	@Synchronized
+	override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+		if (!loaded) fillCacheWithPersistedCookies()
+
+		cookieCache.addAll(cookies)
+		cookiePersistor.saveAll(
+			cookies.filter { it.persistent }
+		)
+	}
+
+	@Synchronized
+	override fun loadForRequest(url: HttpUrl): List<Cookie> {
+		if (!loaded) fillCacheWithPersistedCookies()
+
+		val expiredCookies = mutableListOf<Cookie>()
+		val validCookies = mutableListOf<Cookie>()
+
+		cookieCache.forEach { cookie ->
+			if (cookie.isExpired()) expiredCookies.add(cookie)
+			else if (cookie.matches(url)) validCookies.add(cookie)
+		}
+
+		cookieCache.removeAll(expiredCookies)
+		cookiePersistor.removeAll(expiredCookies)
+
+		return validCookies
+	}
+
+	@Synchronized
+	override fun clear() {
+		cookieCache.clear()
+		cookiePersistor.clear()
+	}
+
+	@Synchronized
+	override fun clearSession() {
+		cookieCache.clear()
+		fillCacheWithPersistedCookies()
+	}
+
+	private fun fillCacheWithPersistedCookies() {
+		cookieCache.addAll(cookiePersistor.loadAll())
+		loaded = true
+	}
+}
+
+fun Cookie.isExpired(): Boolean =
+	expiresAt < System.currentTimeMillis()
