42f2268c042b7d561f52868ad44107f7d9b9b7bd
Justin Vartanian <jvartanian@1904labs.com>
2020-06-09 09:32:39 -0500

    More docs

--- network/src/main/java/com/labs1904/network/auth/TokenApi.kt
+++ network/src/main/java/com/labs1904/network/auth/TokenApi.kt
@@ -2,6 +2,17 @@ package com.labs1904.network.auth
 
 import io.reactivex.rxjava3.core.Single
 
+/**
+ * Represents an API to refresh authentication tokens. This interface can be extended to add additional
+ * token related API calls.
+ */
 interface TokenApi<T : Tokens> {
+	/**
+	 * Refresh the expired authentication token
+	 *
+	 * @param tokens The current tokens
+	 *
+	 * @return A Single representing the API call. Upon success, a new Tokens object will be emitted
+	 */
 	fun refreshToken(tokens: Tokens): Single<T>
 }
--- network/src/main/java/com/labs1904/network/auth/TokenDataSource.kt
+++ network/src/main/java/com/labs1904/network/auth/TokenDataSource.kt
@@ -3,12 +3,29 @@ package com.labs1904.network.auth
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Maybe
 
+/**
+ * Represents a mechanism to store and retrieve an authentication token.
+ */
 interface TokenDataSource<T : Tokens> {
 
+	/**
+	 * Retrieve the current authentication token
+	 *
+	 * @return A Maybe that represents the operation to retrieve the current token
+	 */
 	fun currentTokens(): Maybe<T>
 
+	/**
+	 * Insert a new authentication token
+	 *
+	 * @return A Completable that will complete when the new token has been successfully stored
+	 */
 	fun insertTokens(tokens: T): Completable
 
+	/**
+	 * Remove the current authentication token from this data source.
+	 *
+	 * @return A Completable that will complete when the token is cleared
+	 */
 	fun clear(): Completable
-
 }
