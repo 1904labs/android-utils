3d7d6ab682c16681a9eee4a60107171ecdc6270e
Justin Vartanian <jvartanian@1904labs.com>
2020-06-09 15:50:07 -0500

    Use less mock objects

--- network/src/test/java/com/labs1904/network/auth/AuthInterceptorTest.kt
+++ network/src/test/java/com/labs1904/network/auth/AuthInterceptorTest.kt
@@ -2,72 +2,61 @@ package com.labs1904.network.auth
 
 import com.labs1904.network.AUTHORIZATION
 import com.labs1904.network.BEARER
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.times
-import com.nhaarman.mockitokotlin2.verify
-import com.nhaarman.mockitokotlin2.whenever
+import com.nhaarman.mockitokotlin2.*
 import io.reactivex.rxjava3.core.Maybe
 import okhttp3.Interceptor
 import okhttp3.Request
-import okhttp3.Response
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class AuthInterceptorTest {
 
-	private lateinit var testObject: AuthInterceptor
-
 	@Test
 	fun `TokenDataSource is not accessed when Authorization header exists`() {
-		val request: Request = mock()
-		val expectedResponse: Response = mock()
+		val accessToken = "A12345667B"
+		val url = "https://google.com/"
+
+		val request = Request.Builder()
+			.url(url)
+			.header(AUTHORIZATION, "$BEARER $accessToken")
+			.build()
+
+
 		val chain: Interceptor.Chain = mock()
 		val tokenData: TokenDataSource<Tokens> = mock()
 
-		whenever(request.header(AUTHORIZATION)).thenReturn("$BEARER A12345667B")
-
 		whenever(chain.request()).thenReturn(request)
-		whenever(chain.proceed(request)).thenReturn(expectedResponse)
+		whenever(chain.proceed(request)).thenReturn(mock())
 
-		testObject = AuthInterceptor(tokenData)
-
-		val actualResponse = testObject.intercept(chain)
+		AuthInterceptor(tokenData).intercept(chain)
 
 		verify(tokenData, times(0)).currentTokens()
-		assertEquals(expectedResponse, actualResponse)
 	}
 
 	@Test
 	fun `new request is created when Authorization header is missing`() {
-		val newAccessToken = "A12345667B"
+		val accessToken = "A12345667B"
+		val url = "https://google.com/"
 
-		val originalRequest: Request = mock()
-		val updatedRequest: Request = mock()
-		val expectedResponse: Response = mock()
-		val requestBuilder: Request.Builder = mock()
+		val originalRequest = Request.Builder()
+			.url(url)
+			.build()
+
 		val chain: Interceptor.Chain = mock()
 		val tokenData: TokenDataSource<Tokens> = mock()
-		val tokenMaybe: Maybe<Tokens> = mock()
 		val newTokens: Tokens = mock()
 
 		whenever(chain.request()).thenReturn(originalRequest)
+		whenever(chain.proceed(any())).thenReturn(mock())
 
-		whenever(originalRequest.header(AUTHORIZATION)).thenReturn(null)
+		whenever(tokenData.currentTokens()).thenReturn(Maybe.just(newTokens))
+		whenever(newTokens.accessToken).thenReturn(accessToken)
 
-		whenever(tokenData.currentTokens()).thenReturn(tokenMaybe)
-		whenever(tokenMaybe.blockingGet()).thenReturn(newTokens)
-		whenever(newTokens.accessToken).thenReturn(newAccessToken)
+		AuthInterceptor(tokenData).intercept(chain)
 
-		whenever(originalRequest.newBuilder()).thenReturn(requestBuilder)
-		whenever(requestBuilder.addHeader(AUTHORIZATION, "$BEARER $newAccessToken")).thenReturn(requestBuilder)
-		whenever(requestBuilder.build()).thenReturn(updatedRequest)
-
-		whenever(chain.proceed(updatedRequest)).thenReturn(expectedResponse)
-
-		testObject = AuthInterceptor(tokenData)
-
-		val actualResponse = testObject.intercept(chain)
-
-		assertEquals(expectedResponse, actualResponse)
+		val requestCaptor = argumentCaptor<Request>()
+		verify(chain).proceed(requestCaptor.capture())
+		assertEquals("$BEARER $accessToken", requestCaptor.firstValue.header(AUTHORIZATION))
+		assertEquals(url, requestCaptor.firstValue.url.toString())
 	}
 }
