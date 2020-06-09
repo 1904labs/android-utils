8028170474c54961ab8710c8995d0641519855e1
Justin Vartanian <jvartanian@1904labs.com>
2020-06-08 20:58:36 -0500

    Add CookieCache docs

--- network/src/main/java/com/labs1904/network/cookies/cache/CookieCache.kt
+++ network/src/main/java/com/labs1904/network/cookies/cache/CookieCache.kt
@@ -2,6 +2,9 @@ package com.labs1904.network.cookies.cache
 
 import com.labs1904.network.cookies.SerializableCookie
 
+/**
+ * A CookieCache object represents a storage mechanism for cookies.
+ */
 interface CookieCache {
 	fun insert(cookie: SerializableCookie)
 	fun insertAll(cookies: List<SerializableCookie>)
@@ -11,6 +14,14 @@ interface CookieCache {
 	fun clear()
 }
 
+/**
+ * A SessionCookieCache object represents a storage mechanism for session cookies. Session cookies should be stored in memory
+ * and never written to disk. They are valid only for the current session, and will be lost when the application process is killed.
+ */
 interface SessionCookieCache : CookieCache
 
+/**
+ * A PersistentCookieCache object represents a storage mechanism for persistent cookies. Persistent cookies have an expiration date,
+ * and should be stored to disk, only being removed when they have expired.
+ */
 interface PersistentCookieCache : CookieCache
