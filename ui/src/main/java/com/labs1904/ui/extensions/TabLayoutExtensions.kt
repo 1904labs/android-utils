33875ddd4bc25511dc760d12aa32b850fe99b6e2
Justin McCann <justin.mccann@1904labs.com>
2020-06-15 10:21:09 -0500

    Added java docs for TabLayout.

--- ui/src/main/java/com/labs1904/ui/extensions/TabLayoutExtensions.kt
+++ ui/src/main/java/com/labs1904/ui/extensions/TabLayoutExtensions.kt
@@ -2,6 +2,12 @@ package com.labs1904.ui.extensions
 
 import com.google.android.material.tabs.TabLayout
 
+/**
+ * Easily add a tab selected listener to a TabLayout without all of the boilerplate code.
+ *
+ * @param listener A function that has contains a Tab as a parameter and performs any logic you would
+ * like to run once a Tab is selected.
+ */
 fun TabLayout.addOnTabSelectedListener(listener: (TabLayout.Tab?) -> Unit) {
     addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
         override fun onTabReselected(tab: TabLayout.Tab?) = Unit
