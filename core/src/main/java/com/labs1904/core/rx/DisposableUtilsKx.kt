54143b42f636ba0fa344daf907ed05e734bfe6a6
Justin Vartanian <jvartanian@1904labs.com>
2020-06-15 11:55:42 -0500

    Setup unit tests

--- core/src/main/java/com/labs1904/core/rx/DisposableUtilsKx.kt
+++ core/src/main/java/com/labs1904/core/rx/DisposableUtilsKx.kt
@@ -3,9 +3,9 @@ package com.labs1904.core.rx
 import io.reactivex.rxjava3.disposables.Disposable
 
 /**
- * Return true if this Disposable is null or has been disposed.
+ * Checks whether this Disposable instance is null or has been disposed.
  *
- * @return true if this Disposable is null or has been disposed.
+ * @return true if this Disposable is null or has been disposed, false otherwise.
  */
 fun Disposable?.isNullOrDisposed(): Boolean =
 	this == null || this.isDisposed
